ee544c7d4a0c51e58498fbdd25d8972bc50decd7
tr3n4bol <tr3n4bol@gmail.com>
2026-05-28 14:47:52 +0300

    online status socket rework

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
 const authRouter = require("./controllers/authController");
 const userRouter = require("./controllers/userController");
 const chatRouter = require("./controllers/chatController");
 const messageRouter = require("./controllers/messageController");
-const message = require("./models/message");
+
+const allowedOrigins = [
+    "http://localhost:3000"
+];
 
 app.use(express.json({ limit: "50mb" }));
 
+app.use(
+    cors({
+        origin: allowedOrigins,
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    }),
+);
+
 const server = require("http").createServer(app);
+
 const io = require("socket.io")(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: allowedOrigins,
         methods: ["GET", "POST"],
+        credentials: true,
     },
 });
 
-const onlineUsers = [];
+const onlineUsers = new Map();
+
+const getOnlineUserIds = () => {
+    return Array.from(onlineUsers.keys());
+};
+
+const emitOnlineUsers = () => {
+    io.emit("online-users-updated", getOnlineUserIds());
+};
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
@@ -26,6 +51,35 @@ app.use("/api/message", messageRouter);
 io.on("connection", (socket) => {
     socket.on("join-room", (userId) => {
         socket.join(userId);
+    });
+
+    socket.on("user-login", (userId) => {
+        if (!userId) return;
+
+        if (!onlineUsers.has(userId)) {
+            onlineUsers.set(userId, new Set());
+        }
+
+        onlineUsers.get(userId).add(socket.id);
+        socket.data.userId = userId;
+
+        emitOnlineUsers();
+    });
+
+    socket.on("user-logout", (userId) => {
+        if (!userId) return;
+
+        const userSockets = onlineUsers.get(userId);
+
+        if (userSockets) {
+            userSockets.delete(socket.id);
+
+            if (userSockets.size === 0) {
+                onlineUsers.delete(userId);
+            }
+        }
+
+        emitOnlineUsers();
     });
 
     socket.on("send-message", (data) => {
@@ -48,18 +102,24 @@ io.on("connection", (socket) => {
         io.to(data.members[0]).to(data.members[1]).emit("started-typing", data);
     });
 
-    // TODO
-    // Fix online users
-    socket.on("user-login", (userId) => {
-        if (!onlineUsers.includes(userId)) {
-            onlineUsers.push(userId);
-        }
-        socket.emit("online-users", onlineUsers);
-    });
+    socket.on("disconnect", () => {
+        const userId = socket.data.userId;
 
-    socket.on("user-logout", (userId) => {
-        onlineUsers.splice(onlineUsers.indexOf(userId), 1);
-        io.emit("online-users-updated", onlineUsers);
+        if (!userId) {
+            return;
+        }
+
+        const userSockets = onlineUsers.get(userId);
+
+        if (userSockets) {
+            userSockets.delete(socket.id);
+
+            if (userSockets.size === 0) {
+                onlineUsers.delete(userId);
+            }
+        }
+
+        emitOnlineUsers();
     });
 });
 
